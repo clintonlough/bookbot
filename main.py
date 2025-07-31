b1c5b0a648a313df4ce90a0fb080fdea63c10c3c
clintonlough <clinton.lough@gmail.com>
2025-07-31 19:58:43 +1000

    finished project - accepts sys.argv

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sys
 from stats import *
+
+def check_args():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 def get_book_text(filepath):
     file_contents = ""
@@ -22,7 +28,10 @@ def print_report(filepath, words, chars_sorted):
     print("============= END ===============")
 
 def main():
-    filepath = "books/frankenstein.txt"
+
+    check_args()
+
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     word_count = count_words(book_text)
     char_count = count_characters(book_text)
